efe99a611d78159188c433799efa6e6de404d702
ricigeroi <andrey.bardier@gmail.com>
2024-12-13 17:57:50 +0200

    add task f

---
+++ f_ProductofArrayExceptSelf/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        arr = [1] * n
+
+        left, right = 1, 1
+
+        for i in range(n):
+            arr[i] = left
+            left = left * nums[i]
+
+        for i in range(n - 1, -1, -1):
+            arr[i] = arr[i] * right
+            right = right * nums[i]
+
+        return arr
