acb62bfcb416df6baba1727e62d30a0c43f45360
ricigeroi <andrey.bardier@gmail.com>
2024-12-13 19:27:21 +0200

    add task g (Maximum Subarray)

---
+++ g_MaximumSubarray/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        current_sum = max_sum
+
+        for item in nums[1:]:
+            current_sum = max(item, current_sum + item)
+            max_sum = max(current_sum, max_sum)
+
+        return max_sum
