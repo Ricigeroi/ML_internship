8f55c911f2901c920e7cf6208a8d9dce21595f94
abardier <abardier@griddynamics.com>
2024-12-16 15:46:02 +0200

    add task q (Number Of Islands)

---
+++ q_NumberOfIslands/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        n = len(grid)
+        m = len(grid[0])
+        def dfs(i, j):
+            grid[i][j] = '0'
+            for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if (0 <= x < n) and (0 <= y < m) and (grid[x][y] == '1'):
+                    dfs(x, y)
+
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    count += 1
+                    dfs(i, j)
+
+        return count
