a88348aa858f242d2b204e6bfbb5c3180236a376
ricigeroi <andrey.bardier@gmail.com>
2024-12-26 21:04:02 +0200

    add multi-files upload

--- Module4_Flask/app.py
+++ Module4_Flask/app.py
@@ -12,6 +12,8 @@ if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 app = Flask(__name__)
 app.config['UPLOAD_DIRECTORY'] = UPLOAD_DIRECTORY
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 
 
 def allowed_file(filename):
@@ -40,21 +42,42 @@ def post_files():
     if 'file' not in request.files:
         return make_response(jsonify({"error": "No file part in the request"}), 400)
 
-    file = request.files['file']
+    files = request.files.getlist('file')
+    responses = []
 
-    if file.filename == '':
-        return make_response(jsonify({"error": "No file selected"}), 400)
+    for file in files:
+        if file.filename == '':
+            responses.append({"filename": None, "error": "No file selected"})
+            continue
 
-    if not allowed_file(file.filename):
-        return make_response(jsonify({"error": "File type not allowed"}), 415)
+        if not allowed_file(file.filename):
+            responses.append({"filename": file.filename, "error": "File type not allowed"})
+            continue
 
+        try:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(UPLOAD_DIRECTORY, filename)
+
+            if os.path.exists(file_path):
+                responses.append({"filename": filename, "error": "File already exists"})
+                continue
+
+            file.save(file_path)
+            responses.append({"filename": filename, "message": "File uploaded successfully"})
+        except Exception as e:
+            responses.append({"filename": file.filename, "error": f"Failed to save the file: {str(e)}"})
+
+    return jsonify(responses), 207
+
+
+@app.route('/files/<name>', methods=['DELETE'])
+def delete_file(name):
     try:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_DIRECTORY, filename))
+        os.remove(os.path.join(UPLOAD_DIRECTORY, name))
+        return jsonify({"message": "File deleted successfully"}), 200
     except Exception as e:
-        return make_response(jsonify({"error": f"Failed to save the file: {str(e)}"}), 500)
+        return make_response(jsonify({"error": f"Failed to delete file: {str(e)}"}), 409)
 
-    return jsonify({"message": "File uploaded successfully", "filename": filename}), 201
 
 if __name__ == '__main__':
     app.run()
